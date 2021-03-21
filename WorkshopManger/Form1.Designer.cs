963640a5ea7cd5ebac775a986303daee178493da
Leena <Leeno448@gmail.com>
2021-03-21 16:25:30 +0300

    Add new employee form design

--- WorkshopManger/Form1.Designer.cs
+++ WorkshopManger/Form1.Designer.cs
@@ -29,12 +29,182 @@ namespace WorkshopManger
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Phone_textbox = new System.Windows.Forms.TextBox();
+            this.Password_textbox = new System.Windows.Forms.TextBox();
+            this.Email_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LName_textbox = new System.Windows.Forms.TextBox();
+            this.FName_textbox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(259, 326);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(352, 43);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(120, 274);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Phone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(120, 216);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(120, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email:";
+            // 
+            // Phone_textbox
+            // 
+            this.Phone_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Phone_textbox.Location = new System.Drawing.Point(259, 272);
+            this.Phone_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Phone_textbox.Name = "Phone_textbox";
+            this.Phone_textbox.Size = new System.Drawing.Size(352, 32);
+            this.Phone_textbox.TabIndex = 5;
+            // 
+            // Password_textbox
+            // 
+            this.Password_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Password_textbox.Location = new System.Drawing.Point(259, 214);
+            this.Password_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Password_textbox.Name = "Password_textbox";
+            this.Password_textbox.Size = new System.Drawing.Size(352, 32);
+            this.Password_textbox.TabIndex = 4;
+            // 
+            // Email_textbox
+            // 
+            this.Email_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Email_textbox.Location = new System.Drawing.Point(259, 158);
+            this.Email_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Email_textbox.Name = "Email_textbox";
+            this.Email_textbox.Size = new System.Drawing.Size(352, 32);
+            this.Email_textbox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(120, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(120, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Frist Name:";
+            // 
+            // LName_textbox
+            // 
+            this.LName_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LName_textbox.Location = new System.Drawing.Point(259, 99);
+            this.LName_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LName_textbox.Name = "LName_textbox";
+            this.LName_textbox.Size = new System.Drawing.Size(352, 32);
+            this.LName_textbox.TabIndex = 0;
+            // 
+            // FName_textbox
+            // 
+            this.FName_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FName_textbox.Location = new System.Drawing.Point(259, 45);
+            this.FName_textbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FName_textbox.Name = "FName_textbox";
+            this.FName_textbox.Size = new System.Drawing.Size(352, 32);
+            this.FName_textbox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Phone_textbox);
+            this.groupBox1.Controls.Add(this.Password_textbox);
+            this.groupBox1.Controls.Add(this.Email_textbox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.LName_textbox);
+            this.groupBox1.Controls.Add(this.FName_textbox);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(781, 427);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new employee";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Phone_textbox;
+        private System.Windows.Forms.TextBox Password_textbox;
+        private System.Windows.Forms.TextBox Email_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LName_textbox;
+        private System.Windows.Forms.TextBox FName_textbox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
