7f6f10bdeafde1c814669033a58c976efcfa7202
Ranim1997 <65516141+Ranim1997@users.noreply.github.com>
2021-03-22 12:58:26 +0300

    Add Organizer

---
+++ WorkshopManger/AddOrganizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WorkshopManger
+{
+    public partial class AddOrganizer : Form
+    {
+        public AddOrganizer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
