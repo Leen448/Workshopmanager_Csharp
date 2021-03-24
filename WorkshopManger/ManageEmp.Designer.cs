7454c063c17d11c2c0e5a6d5231e524f83da42d6
Leena <Leeno448@gmail.com>
2021-03-24 15:31:50 +0300

    Update manageEmp form

--- WorkshopManger/ManageEmp.Designer.cs
+++ WorkshopManger/ManageEmp.Designer.cs
@@ -30,8 +30,8 @@ namespace WorkshopManger
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Delete_button = new System.Windows.Forms.Button();
+            this.Update_button = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,8 +47,8 @@ namespace WorkshopManger
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Delete_button);
+            this.groupBox1.Controls.Add(this.Update_button);
             this.groupBox1.Controls.Add(this.dataGridView1);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label3);
@@ -58,51 +58,57 @@ namespace WorkshopManger
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.LName_textbox);
             this.groupBox1.Controls.Add(this.FName_textbox);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(15, 17);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(774, 424);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(885, 565);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Manage employee";
             // 
-            // button2
+            // Delete_button
             // 
-            this.button2.Location = new System.Drawing.Point(401, 357);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Delete";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Delete_button.Location = new System.Drawing.Point(458, 476);
+            this.Delete_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Delete_button.Name = "Delete_button";
+            this.Delete_button.Size = new System.Drawing.Size(86, 31);
+            this.Delete_button.TabIndex = 12;
+            this.Delete_button.Text = "Delete";
+            this.Delete_button.UseVisualStyleBackColor = true;
+            this.Delete_button.Click += new System.EventHandler(this.Delete_button_click);
             // 
-            // button1
+            // Update_button
             // 
-            this.button1.Location = new System.Drawing.Point(288, 357);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Update";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Update_button.Location = new System.Drawing.Point(329, 476);
+            this.Update_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Update_button.Name = "Update_button";
+            this.Update_button.Size = new System.Drawing.Size(86, 31);
+            this.Update_button.TabIndex = 11;
+            this.Update_button.Text = "Update";
+            this.Update_button.UseVisualStyleBackColor = true;
+            this.Update_button.Click += new System.EventHandler(this.Update_button_click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(49, 154);
+            this.dataGridView1.Location = new System.Drawing.Point(56, 233);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(652, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(745, 200);
             this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmp_CellContentClick);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(387, 93);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(442, 124);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(59, 19);
+            this.label5.Size = new System.Drawing.Size(72, 24);
             this.label5.TabIndex = 8;
             this.label5.Text = "Phone:";
             // 
@@ -110,39 +116,39 @@ namespace WorkshopManger
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(49, 88);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(56, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 19);
+            this.label3.Size = new System.Drawing.Size(66, 24);
             this.label3.TabIndex = 6;
             this.label3.Text = "Email:";
             // 
             // Phone_textbox
             // 
             this.Phone_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Phone_textbox.Location = new System.Drawing.Point(508, 90);
-            this.Phone_textbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Phone_textbox.Location = new System.Drawing.Point(581, 120);
+            this.Phone_textbox.Margin = new System.Windows.Forms.Padding(5);
             this.Phone_textbox.Name = "Phone_textbox";
-            this.Phone_textbox.Size = new System.Drawing.Size(193, 27);
+            this.Phone_textbox.Size = new System.Drawing.Size(220, 32);
             this.Phone_textbox.TabIndex = 5;
             // 
             // Email_textbox
             // 
             this.Email_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Email_textbox.Location = new System.Drawing.Point(170, 85);
-            this.Email_textbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Email_textbox.Location = new System.Drawing.Point(194, 113);
+            this.Email_textbox.Margin = new System.Windows.Forms.Padding(5);
             this.Email_textbox.Name = "Email_textbox";
-            this.Email_textbox.Size = new System.Drawing.Size(193, 27);
+            this.Email_textbox.Size = new System.Drawing.Size(220, 32);
             this.Email_textbox.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(387, 41);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(442, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.Size = new System.Drawing.Size(112, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Last Name:";
             // 
@@ -151,39 +157,41 @@ namespace WorkshopManger
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(49, 40);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(56, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.Size = new System.Drawing.Size(113, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Frist Name:";
             // 
             // LName_textbox
             // 
             this.LName_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LName_textbox.Location = new System.Drawing.Point(508, 38);
-            this.LName_textbox.Margin = new System.Windows.Forms.Padding(4);
+            this.LName_textbox.Location = new System.Drawing.Point(581, 51);
+            this.LName_textbox.Margin = new System.Windows.Forms.Padding(5);
             this.LName_textbox.Name = "LName_textbox";
-            this.LName_textbox.Size = new System.Drawing.Size(193, 27);
+            this.LName_textbox.Size = new System.Drawing.Size(220, 32);
             this.LName_textbox.TabIndex = 0;
             // 
             // FName_textbox
             // 
             this.FName_textbox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FName_textbox.Location = new System.Drawing.Point(170, 38);
-            this.FName_textbox.Margin = new System.Windows.Forms.Padding(4);
+            this.FName_textbox.Location = new System.Drawing.Point(194, 51);
+            this.FName_textbox.Margin = new System.Windows.Forms.Padding(5);
             this.FName_textbox.Name = "FName_textbox";
-            this.FName_textbox.Size = new System.Drawing.Size(193, 27);
+            this.FName_textbox.Size = new System.Drawing.Size(220, 32);
             this.FName_textbox.TabIndex = 0;
             // 
             // ManageEmp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ManageEmp";
             this.Text = "ManageEmp";
+            this.Load += new System.EventHandler(this.FRM_ManageEmp_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -194,7 +202,7 @@ namespace WorkshopManger
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Update_button;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
@@ -204,6 +212,6 @@ namespace WorkshopManger
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox LName_textbox;
         private System.Windows.Forms.TextBox FName_textbox;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Delete_button;
     }
 }
--- WorkshopManger/ManageEmp.cs
+++ WorkshopManger/ManageEmp.cs
@@ -10,14 +10,96 @@ namespace WorkshopManger
 {
     public partial class ManageEmp : Form
     {
+        OrgEmployee emp = new OrgEmployee();
         public ManageEmp()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void FRM_ManageEmp_Load(object sender, EventArgs e)
         {
+            //call GetAllCustomers function
+            // fill datagridview with returned of function
+            LoadData();
 
         }
+
+        private void LoadData()
+        {
+            OrgEmployee emp = new OrgEmployee();
+            int OrgID = Account.AccountId;
+            MessageBox.Show("OrgID:" + OrgID);
+            DataTable data = emp.GetAllEmpInOrg(OrgID);
+            if (data.Rows.Count != 0)
+            {
+                dataGridView1.DataSource = data;
+            }
+        }
+
+
+
+        private void Update_button_click(object sender, EventArgs e)
+        {
+            int result = emp.UpdateEmp(r, FName_textbox.Text, LName_textbox.Text , Phone_textbox.Text, Email_textbox.Text , Account.AccountId);
+            if (result != 0)
+            {
+                MessageBox.Show("Data Updated Successfully.");
+                LoadData();
+                ClearControls();
+            }
+
+        }
+
+
+        private void Delete_button_click(object sender, EventArgs e)
+        {
+            int result = emp.DeleteEmp(r);
+            if (result != 0)
+            {
+                MessageBox.Show("Data Deleted.");
+                ClearControls();
+                LoadData();
+            }
+
+        }
+
+
+
+        int r;
+        private void dgvEmp_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            r = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            DataTable dt = emp.GetEmployeeById(r);
+            if (dt.Rows.Count != 0)
+            {
+
+
+                DataRow row = dt.Rows[0];
+                FName_textbox.Text = row["FName"].ToString();
+                LName_textbox.Text = row["LName"].ToString();
+                Phone_textbox.Text = row["Phone"].ToString();
+                Email_textbox.Text = row["Email"].ToString();
+            }
+            else {
+                MessageBox.Show("There is no data");
+            }
+
+        }
+
+
+
+
+        private void ClearControls()
+        {
+            FName_textbox.Text = "";
+            LName_textbox.Text = "";
+            Phone_textbox.Text = "";
+            Email_textbox.Text = "";
+        }
+
+
+
+
+
     }
 }
