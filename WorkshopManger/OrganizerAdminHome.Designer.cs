67176d38d457c4f32ffb798db2bfb3ab0afba163
Leena <Leeno448@gmail.com>
2021-03-24 15:33:25 +0300

    update AdminHome

--- WorkshopManger/OrganizerAdminHome.Designer.cs
+++ WorkshopManger/OrganizerAdminHome.Designer.cs
@@ -34,18 +34,18 @@ namespace WorkshopManger
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.logouttoolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.button5 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.View_Audience_button = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.Take_Attendance_button = new System.Windows.Forms.Button();
+            this.Manage_Workshop_button = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Add_New_Employees_button = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Manage_Employees_button = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button8 = new System.Windows.Forms.Button();
+            this.Add_New_Workshop_button = new System.Windows.Forms.Button();
             this.contextMenuStrip4 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
@@ -60,7 +60,7 @@ namespace WorkshopManger
             // MyProfiletoolStripMenuItem1
             // 
             this.MyProfiletoolStripMenuItem1.Name = "MyProfiletoolStripMenuItem1";
-            this.MyProfiletoolStripMenuItem1.Size = new System.Drawing.Size(73, 20);
+            this.MyProfiletoolStripMenuItem1.Size = new System.Drawing.Size(90, 24);
             this.MyProfiletoolStripMenuItem1.Text = "My Profile";
             // 
             // menuStrip1
@@ -71,110 +71,104 @@ namespace WorkshopManger
             this.MyProfiletoolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(708, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(809, 30);
             this.menuStrip1.TabIndex = 7;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // logouttoolStripMenuItem1
             // 
             this.logouttoolStripMenuItem1.Name = "logouttoolStripMenuItem1";
-            this.logouttoolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.logouttoolStripMenuItem1.Size = new System.Drawing.Size(70, 24);
             this.logouttoolStripMenuItem1.Text = "Logout";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(470, 56);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Location = new System.Drawing.Point(537, 75);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(200, 22);
+            this.button5.Size = new System.Drawing.Size(229, 29);
             this.button5.TabIndex = 5;
             this.button5.Text = "Statistics";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // View_Audience_button
             // 
-            this.button7.Location = new System.Drawing.Point(24, 56);
-            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(200, 22);
-            this.button7.TabIndex = 3;
-            this.button7.Text = "View Audience";
-            this.button7.UseVisualStyleBackColor = true;
+            this.View_Audience_button.Location = new System.Drawing.Point(27, 75);
+            this.View_Audience_button.Name = "View_Audience_button";
+            this.View_Audience_button.Size = new System.Drawing.Size(229, 29);
+            this.View_Audience_button.TabIndex = 3;
+            this.View_Audience_button.Text = "View Audience";
+            this.View_Audience_button.UseVisualStyleBackColor = true;
+            this.View_Audience_button.Click += new System.EventHandler(this.View_Audience_button_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.Take_Attendance_button);
             this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button7);
-            this.groupBox1.Location = new System.Drawing.Point(3, 274);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Controls.Add(this.View_Audience_button);
+            this.groupBox1.Location = new System.Drawing.Point(3, 367);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox1.Size = new System.Drawing.Size(695, 132);
+            this.groupBox1.Size = new System.Drawing.Size(794, 176);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Audience";
             // 
-            // button9
+            // Take_Attendance_button
             // 
-            this.button9.Location = new System.Drawing.Point(248, 56);
-            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(200, 22);
-            this.button9.TabIndex = 6;
-            this.button9.Text = "Take Attendance";
-            this.button9.UseVisualStyleBackColor = true;
+            this.Take_Attendance_button.Location = new System.Drawing.Point(283, 75);
+            this.Take_Attendance_button.Name = "Take_Attendance_button";
+            this.Take_Attendance_button.Size = new System.Drawing.Size(229, 29);
+            this.Take_Attendance_button.TabIndex = 6;
+            this.Take_Attendance_button.Text = "Take Attendance";
+            this.Take_Attendance_button.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // Manage_Workshop_button
             // 
-            this.button6.Location = new System.Drawing.Point(24, 56);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(200, 22);
-            this.button6.TabIndex = 3;
-            this.button6.Text = "View Workshop";
-            this.button6.UseVisualStyleBackColor = true;
+            this.Manage_Workshop_button.Location = new System.Drawing.Point(27, 75);
+            this.Manage_Workshop_button.Name = "Manage_Workshop_button";
+            this.Manage_Workshop_button.Size = new System.Drawing.Size(229, 29);
+            this.Manage_Workshop_button.TabIndex = 3;
+            this.Manage_Workshop_button.Text = "Manage Workshop";
+            this.Manage_Workshop_button.UseVisualStyleBackColor = true;
+            this.Manage_Workshop_button.Click += new System.EventHandler(this.Manage_Workshop_button_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(470, 45);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(537, 60);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(200, 22);
+            this.button3.Size = new System.Drawing.Size(229, 29);
             this.button3.TabIndex = 2;
             this.button3.Text = "Statistics";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // Add_New_Employees_button
             // 
-            this.button2.Location = new System.Drawing.Point(251, 45);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 22);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Add New Employees";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Add_New_Employees_button.Location = new System.Drawing.Point(287, 60);
+            this.Add_New_Employees_button.Name = "Add_New_Employees_button";
+            this.Add_New_Employees_button.Size = new System.Drawing.Size(229, 29);
+            this.Add_New_Employees_button.TabIndex = 1;
+            this.Add_New_Employees_button.Text = "Add New Employees";
+            this.Add_New_Employees_button.UseVisualStyleBackColor = true;
+            this.Add_New_Employees_button.Click += new System.EventHandler(this.Add_New_Employees_button_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(470, 56);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(537, 75);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(200, 22);
+            this.button4.Size = new System.Drawing.Size(229, 29);
             this.button4.TabIndex = 5;
             this.button4.Text = "Statistics";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // Manage_Employees_button
             // 
-            this.button1.Location = new System.Drawing.Point(22, 45);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 22);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Manage Employees";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Manage_Employees_button.Location = new System.Drawing.Point(25, 60);
+            this.Manage_Employees_button.Name = "Manage_Employees_button";
+            this.Manage_Employees_button.Size = new System.Drawing.Size(229, 29);
+            this.Manage_Employees_button.TabIndex = 0;
+            this.Manage_Employees_button.Text = "Manage Employees";
+            this.Manage_Employees_button.UseVisualStyleBackColor = true;
+            this.Manage_Employees_button.Click += new System.EventHandler(this.Manage_Employees_button_Click);
             // 
             // flowLayoutPanel1
             // 
@@ -182,49 +176,44 @@ namespace WorkshopManger
             this.flowLayoutPanel1.Controls.Add(this.groupBox2);
             this.flowLayoutPanel1.Controls.Add(this.groupBox3);
             this.flowLayoutPanel1.Controls.Add(this.groupBox1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(1, 23);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1, 31);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(706, 416);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(807, 555);
             this.flowLayoutPanel1.TabIndex = 6;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(3, 2);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Controls.Add(this.Add_New_Employees_button);
+            this.groupBox2.Controls.Add(this.Manage_Employees_button);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox2.Size = new System.Drawing.Size(695, 132);
+            this.groupBox2.Size = new System.Drawing.Size(794, 176);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Organization Employees";
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.Add_New_Workshop_button);
             this.groupBox3.Controls.Add(this.button4);
-            this.groupBox3.Controls.Add(this.button6);
-            this.groupBox3.Location = new System.Drawing.Point(3, 138);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Controls.Add(this.Manage_Workshop_button);
+            this.groupBox3.Location = new System.Drawing.Point(3, 185);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox3.Size = new System.Drawing.Size(695, 132);
+            this.groupBox3.Size = new System.Drawing.Size(794, 176);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Workshop";
             // 
-            // button8
+            // Add_New_Workshop_button
             // 
-            this.button8.Location = new System.Drawing.Point(248, 56);
-            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(200, 22);
-            this.button8.TabIndex = 6;
-            this.button8.Text = "Add New Workshop";
-            this.button8.UseVisualStyleBackColor = true;
+            this.Add_New_Workshop_button.Location = new System.Drawing.Point(283, 75);
+            this.Add_New_Workshop_button.Name = "Add_New_Workshop_button";
+            this.Add_New_Workshop_button.Size = new System.Drawing.Size(229, 29);
+            this.Add_New_Workshop_button.TabIndex = 6;
+            this.Add_New_Workshop_button.Text = "Add New Workshop";
+            this.Add_New_Workshop_button.UseVisualStyleBackColor = true;
+            this.Add_New_Workshop_button.Click += new System.EventHandler(this.Add_New_Workshop_button_Click);
             // 
             // contextMenuStrip4
             // 
@@ -252,12 +241,11 @@ namespace WorkshopManger
             // 
             // OrganizerAdminHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 440);
+            this.ClientSize = new System.Drawing.Size(809, 587);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "OrganizerAdminHome";
             this.Text = "Organizer_Home";
             this.menuStrip1.ResumeLayout(false);
@@ -277,18 +265,18 @@ namespace WorkshopManger
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem logouttoolStripMenuItem1;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button View_Audience_button;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Take_Attendance_button;
+        private System.Windows.Forms.Button Manage_Workshop_button;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Add_New_Employees_button;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Manage_Employees_button;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button Add_New_Workshop_button;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip4;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
--- WorkshopManger/OrganizerAdminHome.cs
+++ WorkshopManger/OrganizerAdminHome.cs
@@ -15,5 +15,45 @@ namespace WorkshopManger
             InitializeComponent();
         }
 
+        private void Manage_Employees_button_Click(object sender, EventArgs e)
+        {
+            ManageEmp emp = new ManageEmp();
+            emp.Tag = this;
+            emp.Show(this);
+            Hide();
+        }
+
+        private void Add_New_Employees_button_Click(object sender, EventArgs e)
+        {
+            AddEmp emp = new AddEmp();
+            emp.Tag = this;
+            emp.Show(this);
+            Hide();
+        }
+
+        private void Manage_Workshop_button_Click(object sender, EventArgs e)
+        {
+          ManageWorkshop ws = new ManageWorkshop();
+            ws.Tag = this;
+            ws.Show(this);
+            Hide();
+        }
+
+        private void Add_New_Workshop_button_Click(object sender, EventArgs e)
+        {
+            AddWorkshop ADDws = new AddWorkshop();
+            ADDws.Tag = this;
+            ADDws.Show(this);
+            Hide();
+        }
+
+        private void View_Audience_button_Click(object sender, EventArgs e)
+        {
+            ViewAudience VA = new ViewAudience();
+            VA.Tag = this;
+            VA.Show(this);
+            Hide();
+
+        }
     }
 }
