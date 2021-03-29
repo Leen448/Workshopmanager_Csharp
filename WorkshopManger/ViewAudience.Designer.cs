b8ace59ad788d6a821a2f320111b9fdd1ce22612
Leena <Leeno448@gmail.com>
2021-03-29 10:50:55 +0300

    update view audience

--- WorkshopManger/ViewAudience.Designer.cs
+++ WorkshopManger/ViewAudience.Designer.cs
@@ -31,6 +31,7 @@ namespace WorkshopManger
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BacktoHome = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -38,9 +39,11 @@ namespace WorkshopManger
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Location = new System.Drawing.Point(1, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(628, 262);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(903, 530);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "View Audience";
@@ -48,20 +51,36 @@ namespace WorkshopManger
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 22);
+            this.dataGridView1.Location = new System.Drawing.Point(11, 28);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(616, 228);
+            this.dataGridView1.Size = new System.Drawing.Size(881, 494);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // BacktoHome
+            // 
+            this.BacktoHome.Location = new System.Drawing.Point(797, 542);
+            this.BacktoHome.Name = "BacktoHome";
+            this.BacktoHome.Size = new System.Drawing.Size(87, 32);
+            this.BacktoHome.TabIndex = 1;
+            this.BacktoHome.Text = "Home";
+            this.BacktoHome.UseVisualStyleBackColor = true;
+            this.BacktoHome.Click += new System.EventHandler(this.BacktoHome_Click);
             // 
             // ViewAudience
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(652, 303);
+            this.ClientSize = new System.Drawing.Size(905, 581);
+            this.Controls.Add(this.BacktoHome);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ViewAudience";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ViewAudience";
+            this.Load += new System.EventHandler(this.ViewAudience_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -72,5 +91,6 @@ namespace WorkshopManger
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BacktoHome;
     }
 }
--- WorkshopManger/ViewAudience.cs
+++ WorkshopManger/ViewAudience.cs
@@ -13,6 +13,53 @@ namespace WorkshopManger
         public ViewAudience()
         {
             InitializeComponent();
+          
         }
+
+        private void BacktoHome_Click(object sender, EventArgs e)
+        {
+                switch (Account.AccountType)
+                {
+                case 1:
+                    Account.AccountType = 1;
+                    OrganizerAdminHome Orgadmin = new OrganizerAdminHome();
+                    Orgadmin.Tag = this;
+                    Orgadmin.Show(this);
+                    Hide();
+                    break;
+                case 2:
+                    Account.AccountType = 2;
+                    OrgnaizerEmpHome OrgEmp = new OrgnaizerEmpHome();
+                    OrgEmp.Tag = this;
+                    OrgEmp.Show(this);
+                    Hide();
+                    break;
+                case 3:
+
+                    Account.AccountType = 3;
+                    AdminHome admin = new AdminHome();
+                    admin.Tag = this;
+                    admin.Show(this);
+                    Hide();
+                    break;
+
+            }
+            
+        }
+
+        private void ViewAudience_Load(object sender, EventArgs e)
+        {
+           Audience aud = new Audience();
+            DataTable data = aud.GetAllAudience();
+            if (data.Rows.Count != 0)
+            {
+                dataGridView1.DataSource = data;
+            }
+
+        }
+
     }
+
+
+
 }
